505e9b3a6e163f14c771cb5dc2e6b5bb7dc42883
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-05 19:27:17 +1000

    ylabells are updated

--- R/Figs.R
+++ R/Figs.R
@@ -3,20 +3,20 @@ theme_set(theme_bw()) # graphic backgroud is white
 ######################
 # summary data frame #
 ######################
-RngMean <- ddply(extrMlt, .(time, date, co2, ring, variable), Crt_SmryDF) 
+RngMean <- ddply(mineMlt, .(time, date, co2, ring, variable), Crt_SmryDF) 
 TrtMean <- TrtMean <- ddply(RngMean, .(time, date, co2, variable), function(x) Crt_SmryDF(x, val = "Mean"))
 
 #################################
 # plot each nutrient separately #
 #################################
-vars <- c("Nitrate", "Ammonium", "Phosphate")
+vars <- c("Nitrification", "N_mineralisation", "P_minerlisation")
 
 RngFg <- dlply(RngMean, .(variable), PltRnghMean)
-fls <- paste("Output//Figs/FACE_Extractable_Ring_", vars, sep = "")
+fls <- paste("output//figs/FACE_mineralisation_Ring_", vars, sep = "")
 l_ply(1:3, function(x) ggsavePP(filename = fls[x], plot = RngFg[[x]], width = 6, height = 3))
 
 TrtFg <- dlply(TrtMean, .(variable), PltCO2Mean)
-fls <- paste("Output//Figs/FACE_Extractable_CO2_", vars, sep = "")
+fls <- paste("output//figs/FACE_mineralisation_CO2_", vars, sep = "")
 l_ply(1:3, function(x) ggsavePP(filename = fls[x], plot = TrtFg[[x]], width = 6, height = 3))
 
 ##################################
@@ -24,10 +24,10 @@ l_ply(1:3, function(x) ggsavePP(filename = fls[x], plot = TrtFg[[x]], width = 6,
 ##################################
 # labels for facet_grid
 ylabs <- list(
-  'no' = expression(NO[3]^"-"-N),
-  'nh' = expression(NH[4]^"+"-N),
-  'po' = expression(PO[4]^"3-"-P))
-
+  'nitrification' = expression(Net~nitrification~rate),
+  'nh' = expression(Net~N_mineralisation~rate),
+  'po' = expression(Net~P_mineralisation~rate)
+  )
 
 ylab_label <- function(variable, value){
   return(ylabs[value])
@@ -35,4 +35,4 @@ ylab_label <- function(variable, value){
 
 pl <- PltCO2Mean(TrtMean) +
   facet_grid(variable~., scales= "free_y", labeller= ylab_label)
-ggsavePP(filename = "output//figs/FACE_IEM_CO2Trt", plot = pl, width = 6, height = 6)
+ggsavePP(filename = "output//figs/FACE_Mineralisation_CO2Trt", plot = pl, width = 6, height = 6)
