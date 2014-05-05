1775ef621426c8cdcda30aad43516c431592c95c
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-05 16:56:12 +1000

    summary table is created

--- R/Analysis.R
+++ R/Analysis.R
@@ -1,8 +1,46 @@
 rm(list=ls(all=TRUE))
 
+library(car)
+library(gmodels)
+library(lme4)
+library(lubridate)
+library(MASS)
+library(nlme)
+library(packrat)
+library(plyr)
+library(reshape)
+library(xlsx)
+library(contrast)
+library(effects)
+library(ggplot2)
+library(xtable)
 
+source("R/function.R")
 
-source("functions/function.R")
+######################
+# Process data frame #
+######################
+mine<-read.csv("Data/mineralisation.kgbais.csv",
+                 colClasses=c("ring"="factor",
+                              "plot"="factor",
+                              "time"="factor",
+                              "coverage" = "NULL"))
 
-result<-read.csv("Data/mineralisation.kgbais.csv",colClasses=c("ring"="factor","plot"="factor","time"="factor", 
-                                                               "coverage" = "NULL"))
+# remove unnecessary rows
+mine <- droplevels(mine[complete.cases(mine), ])
+
+# format date
+mine$insertion <- as.Date(dmy(mine$insertion))
+mine$sampling <- as.Date(dmy(mine$sampling))
+
+# middle date of insertion and sampling
+MeanDate <- apply(cbind(mine$insertion, mine$sampling), 1, mean)
+mine$date <- as.Date(ave(MeanDate, mine$time), origin = origin) # same date for same time
+
+# save
+save(mine, file = "Output//Data/mineralisation.RData")
+
+#######################
+# Excel summary table #
+#######################
+source("R/SummaryExlTable.R")
---
+++ R/SummaryExlTable.R
@@ -0,0 +1,28 @@
+# melt dataset
+mineMlt <- melt(mine, id = names(mine)[which(!(names(mine) %in% c("nitrification", "n.min", "p.min")))])
+
+# Ring summary table & mean
+RngSmmryTbl <- dlply(mineMlt, .(variable), function(x) CreateTable(x, fac = "ring", digit = 3, nsmall = 3))
+RngMean <- ddply(mineMlt, .(time, date, co2, ring, variable), summarise, value = mean(value, na.rm = TRUE)) 
+
+# treat summary table $ mean
+TrtSmmryTbl <- dlply(RngMean, .(variable), function(x) CreateTable(x, fac = "co2",  digit = 3, nsmall =3))
+
+## create xcel workbook ##
+wb <- createWorkbook()
+
+# worksheet for rowdata
+sheet <- createSheet(wb,sheetName="row_data")
+addDataFrame(mine, sheet, showNA=TRUE, row.names=FALSE, characterNA="NA")
+
+# worksheets for ring summary
+vars <- c("Nitrification", "N_mineralisation", "P_mineralisation")
+shnames <- paste("Ring_mean.",vars ,sep="")
+l_ply(1:3, function(x) crSheet(sheetname = shnames[x], dataset = RngSmmryTbl[[x]]))
+
+# worksheets for temp trt summary
+shnames <- paste("CO2_mean.", vars, sep = "")
+l_ply(1:3, function(x) crSheet(sheetname = shnames[x], dataset = TrtSmmryTbl[[x]]))
+
+#save file
+saveWorkbook(wb,"output/table/FACE_mineactable.xlsx")
