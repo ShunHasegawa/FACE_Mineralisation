06d5234ce6f78e466ae1f2f9323b1902e318aac3
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-06-23 17:10:43 +1000

    plot of soil moisutre and temp are added

--- R/Stats.R
+++ R/Stats.R
@@ -1,3 +1,17 @@
+##################
+# Moist and Temp #
+##################
+theme_set(theme_bw())
+
+p <- ggplot(subsetD(mine, time != 1), aes(x = Temp_Max, y = log(Moist), col = ring))
+p2 <- p + geom_point(alpha = .5) 
+
+pl  <- p2 + facet_wrap( ~ ring)
+ggsavePP(file = "output//figs/FACE_mine_SoilVa_Ringr", plot = pl, width = 6, height = 6)
+
+ggsavePP(file = "output//figs/FACE_Mineralisation_SoilVa", plot = p2, width = 6, height = 6)
+
+
 #################
 # Nitrification #
 #################
